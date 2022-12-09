fc0f77640bc3f4e5b1b7bcd672f575d5c09b27b7
Joe Ciesielski <jtcies@gmail.com>
2022-12-08 22:02:23 -0500

    nvim configs

---
+++ .config/nvim/lua/config.lua
@@ -0,0 +1,8 @@
+
+vim.g.dracula_colorterm = 0
+vim.cmd("colorscheme dracula")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.autoindent = true -- maintain indent of current line
+vim.opt.hidden = true -- allows you to hide buffers with unsaved changes without being prompted
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,7 +3,10 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- themes
-  use {'dracula/vim', as = 'dracula'}
+  use {
+      'dracula/vim', 
+      as = 'dracula',
+      }
   use {
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true },

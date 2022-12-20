ea9cfc32661db369a212e4171f4761d1cb9a3c8c
Joe Ciesielski <jtcies@gmail.com>
2022-12-20 11:20:31 -0500

    lsp

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,4 +1,3 @@
-
 vim.g.dracula_colorterm = 0
 vim.cmd("colorscheme dracula")
 vim.opt.number = true
@@ -6,3 +5,19 @@ vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.autoindent = true -- maintain indent of current line
 vim.opt.hidden = true -- allows you to hide buffers with unsaved changes without being prompted
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.colorcolumn = "80"
